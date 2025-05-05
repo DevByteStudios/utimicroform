ca69ee17a76c4a1db65c99c73c722f8cc298b62c
Etec <etec@etec.com>
2025-05-05 10:27:40 -0300

    Estrutura quase pronta

--- script/script.js
+++ script/script.js
@@ -1,12 +1,20 @@
-
+// Pegando os componentes
 const indicators = [...document.getElementsByClassName('indicator')]
-
 const sections = [...document.querySelectorAll('section')];
 const buttonNext = document.getElementById('buttonNext');
 const buttonBack = document.getElementById('buttonBack');
 const indicatorTitle = document.getElementById("indicatorTitle");
 let actualSection = 0;
 
+let formDadosPessoais={
+    nome: null,
+    telefone: null,
+    rua: null,
+    numero:null,
+    complemento:null,
+    bairro:null,
+    cidade:null,
+}
 // Mostrar o indicador atual
 const actualIndicator = () =>{
     indicators.map((indicator,index)=>{
@@ -41,22 +49,46 @@ const exibirSections = () =>{
     actualIndicator();
 } 
 
-const numTel = document.getElementById("numTel");
-numTel.addEventListener("keyup",(e)=>{
-    let valor = numTel.value.replace(/\D/g, '').slice(0, 11); 
-      
-    // while (valor.length < 11) {
-    //   valor += '_';
-    // }
-
-    // Aplica a máscara: (XX) XXXXX-XXXX
-    let formatado = `(${valor.substring(0,2)}) ${valor.substring(2,7)}-${valor.substring(7,11)}`;
-    numTel.value = formatado;
-    let valorNormal = valor.replace(/\D/g, '').slice(0, 11); // Só os números
-    console.log(valorNormal)
-})
-
 buttonNext.addEventListener("click",()=>{
+    // Verifica os campos do formulario na primeira sessão
+    // if(actualSection == 0){
+    //     formDadosPessoais.nome = document.getElementById('txtNome').value;
+    //     if(formDadosPessoais.nome == ''){
+    //         popupMensagem('error','Digite seu nome completo!','Aviso');
+    //         return;
+    //     }
+    //     formDadosPessoais.telefone = document.getElementById('numTel').value;
+    //     const numeroLimpo = formDadosPessoais.telefone.replace(/\D/g, '');
+    //     if(numeroLimpo.length < 10){
+    //         popupMensagem('error','Telefone incompleto! Digite o DDD e o número completo.','Aviso');
+    //         return;
+    //     }
+    //     if(numeroLimpo.length === 10 && numeroLimpo.charAt(2) === '9'){
+    //         popupMensagem('error','Número de celular incompleto! Verifique se digitou o 9 e todos os outros dígitos.','Aviso');
+    //         return;
+    //     }
+    //     formDadosPessoais.rua = document.getElementById('txtEndereco').value;
+    //     if(formDadosPessoais.rua == ''){
+    //         popupMensagem('error','Digite seu endereço!','Aviso');
+    //         return;
+    //     }
+    //     formDadosPessoais.numero = document.getElementById('numNumero').value;
+    //     if(formDadosPessoais.numero == ''){
+    //         popupMensagem('error','Digite o número de sua casa!','Aviso');
+    //         return;
+    //     }
+    //     formDadosPessoais.bairro = document.getElementById('txtBairro').value;
+    //     if(formDadosPessoais.bairro == ''){
+    //         popupMensagem('error','Digite seu bairro!','Aviso');
+    //         return;
+    //     }
+    //     formDadosPessoais.cidade = document.getElementById('txtCidade').value;
+    //     if(formDadosPessoais.cidade == ''){
+    //         popupMensagem('error','Digite sua cidade!','Aviso');
+    //         return;
+    //     }
+    //     formDadosPessoais.complemento = document.getElementById('txtBairro').value;
+    // }
     if(actualSection < 2){
         actualSection += 1;
         if(actualSection == 2){
@@ -88,5 +120,32 @@ buttonBack.addEventListener("click",()=>{
 
 window.addEventListener("load",()=>{
     exibirSections();
-})
+});
+
+// Configuração para o telefone
+const numTel = document.getElementById("numTel");
+numTel.addEventListener("keypress",(e)=>{
+    formatTel()
+});
+
+const formatTel = () =>{
+    let valor = numTel.value.replace(/\D/g, '').slice(0, 11); 
+      
+    // while (valor.length < 11) {
+    //   valor += '_';
+    // }
+
+    // Aplica a máscara: (XX) XXXXX-XXXX]
+    let formatado = `(${valor.substring(0,2)})${valor.substring(2,7)}-${valor.substring(7,11)}`;
+    numTel.value = formatado;
+    let valorNormal = valor.replace(/\D/g, '').slice(0, 11); // Só os números
+}
+
+const popupMensagem = (icon,txtMsg,txtTitle) =>{
+    Swal.fire({
+        title: txtTitle,
+        text: txtMsg,
+        icon: icon,
+      });
+}
 
